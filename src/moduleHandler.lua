4ce5c95764308d39ef2ce16cc260fe1611a65fd3
NotSoClassy <NotSoClassy357@gmail.com>
2020-12-02 13:20:24 -0500

    disable and enable functions

--- src/moduleHandler.lua
+++ src/moduleHandler.lua
@@ -1,22 +1,49 @@
- -- I stole this idea from Yot https://github.com/object-Object/Yot
 local loader = require './loader'
+local json = require 'json'
 local util = require './util'
+
 local handler = {
-	modules = {}
+	modules = {},
+	moduleNames = {}
 }
 
 function handler.load()
 	local mods = loader.load('modules')
 	for _, mod in ipairs(mods) do
 		table.insert(handler.modules, mod)
+		handler.moduleNames[mod.name] = {name = mod.name, description = mod.description}
 	end
+end
+
+function handler.enable(mod, guild, settings, conn)
+	if not settings.disabled_modules[mod] then return end
+
+	local disabled = settings.disabled_modules
+	settings.disabled_modules[mod] = nil
+
+	local encoded = json.encode(disabled)
+	local stmt = conn:prepare('UPDATE guild_settings SET disabled_modules = ? WHERE guild_id = ?;')
+	stmt:reset():bind(encoded, guild.id):step()
+	stmt:close()
+end
+
+function handler.disable(mod, guild, settings, conn)
+	if settings.disabled_modules[mod] then return end
+
+	local disabled = settings.disabled_modules
+	settings.disabled_modules[mod] = true
+
+	local encoded = json.encode(disabled)
+	local stmt = conn:prepare('UPDATE guild_settings SET disabled_modules = ? WHERE guild_id = ?;')
+	stmt:reset():bind(encoded, guild.id):step()
+	stmt:close()
 end
 
 function handler.runEvent(event, guild, conn, ...)
 	local settings = util.getGuildSettings(guild.id, conn)
 	local success, err = pcall(function(...)
 		for _, mod in ipairs(handler.modules) do
-			if mod.event == event then
+			if mod.event == event and not settings.disabled_modules[mod.name] then
 				mod.run(..., settings, conn)
 			end
 		end
