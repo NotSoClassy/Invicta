774932bc5e432e63a02c82d4e49f3336d225f3d0
NotSoClassy <NotSoClassy357@gmail.com>
2020-12-02 13:19:26 -0500

    better than using settings

---
+++ src/commands/settings/prefix.lua
@@ -0,0 +1,17 @@
+return {
+	name = 'prefix',
+	description = 'Change the bot prefix for the guild',
+	example = '<prefix (can be in quotes)>',
+	execute = function(msg, args, settings, conn)
+		local prefix = table.concat(args, ' '):gsub('^"', ''):gsub('"$', '')
+
+		if #prefix == 0 then return msg:reply('No prefix found') end
+		if prefix == settings.prefix then return msg:reply('The prefix is already `' .. prefix .. '`') end
+
+		local stmt = conn:prepare('UPDATE guild_settings SET prefix = ? WHERE guild_id = ?;')
+		stmt:reset():bind(prefix, msg.guild.id):step()
+		stmt:close()
+
+		return msg:reply('The prefix has been changed to `' .. prefix .. '`')
+	end
+}
