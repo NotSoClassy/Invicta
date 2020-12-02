14d758a13055f5b0bdef187565b6aed69eec2241
NotSoClassy <NotSoClassy357@gmail.com>
2020-12-02 13:20:34 -0500

    json coloums

--- src/util.lua
+++ src/util.lua
@@ -1,15 +1,24 @@
+local json = require 'json'
 local util = {}
+
+local jsonColoums = {
+	disabled_modules = true,
+	disabled_commands = true
+}
 
 local function format(tbl)
 	if type(tbl) ~= 'table' then return end
-	for i,v in pairs(tbl) do
-		tbl[i] = v[1]
+	for i, v in pairs(tbl) do
+		if jsonColoums[i] then
+			tbl[i] = json.parse(v[1])
+		else
+			tbl[i] = v[1]
+		end
 	end
 	return tbl
 end
 
 function util.getGuildSettings(id, conn)
-	if not id then return {} end
 	local settings = conn:exec('SELECT * FROM guild_settings WHERE guild_id=' .. id .. ';', 'k')
 	return format(settings)
 end
