940030d9fa462376f50a353d2a0f1332e9a30db2
NotSoClassy <NotSoClassy357@gmail.com>
2020-12-03 07:28:03 -0500

    if not prefix is found then it tells current prefix

--- src/commands/settings/prefix.lua
+++ src/commands/settings/prefix.lua
@@ -1,11 +1,12 @@
 return {
 	name = 'prefix',
 	description = 'Change the bot prefix for the guild',
-	example = '<prefix (can be in quotes)>',
+	example = '[prefix (can be in quotes)]',
+	userPerms = {'administrator'},
 	execute = function(msg, args, settings, conn)
 		local prefix = table.concat(args, ' '):gsub('^"', ''):gsub('"$', '')
 
-		if #prefix == 0 then return msg:reply('No prefix found') end
+		if #prefix == 0 then return msg:reply('The current prefix is `' .. settings.prefix .. '`') end
 		if prefix == settings.prefix then return msg:reply('The prefix is already `' .. prefix .. '`') end
 
 		local stmt = conn:prepare('UPDATE guild_settings SET prefix = ? WHERE guild_id = ?;')
