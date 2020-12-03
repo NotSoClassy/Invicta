d043ecbccb0011e6a7e8d7ab9c04526a8ca39972
NotSoClassy <NotSoClassy357@gmail.com>
2020-12-03 03:31:01 -0500

    stats command

---
+++ src/commands/stats.lua
@@ -0,0 +1,17 @@
+local toast = require 'toast'
+
+return {
+	name = 'stats',
+	description = 'Get info on the bot',
+	execute = function(msg)
+		local client = msg.client
+		return toast.Embed()
+			:setTitle('Info on ' .. client.user.name)
+			:addField('Guilds:', tostring(#client.guilds), true)
+			:addField('Shards:', (tostring(client.shardCount) or '1') .. ' / ' .. (tostring(client.totalShardCount) or '1'), true)
+			:addField('Uptime:', toast.util.formatLongfunction(math.floor(msg.client.uptime:getTime():toMilliseconds())), true)
+			:setFooter(client.user.name .. ' has ' .. #client.commands .. ' commands')
+			:setColor('DARK_AQUA')
+			:send(msg.channel)
+	end
+}
