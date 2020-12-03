a12258a4127d1418652517acb795c6b2c9e3318f
NotSoClassy <NotSoClassy357@gmail.com>
2020-12-03 15:03:52 -0500

    use bulkdelete

--- src/commands/cleanup.lua
+++ src/commands/cleanup.lua
@@ -1,12 +1,14 @@
 return {
 	name = 'cleanup',
-	description = 'Clear bot messages.',
+	description = 'Clear bot messages and messages that start with the bot prefix.',
 	userPerms = {'manageMessages'},
-	execute = function(msg)
-		for m in msg.channel:getMessages(100) do
-			if m.author == m.client.user then
-				m:delete()
+	execute = function(msg, _, settings)
+		local ids = {}
+		for m in msg.channel:getMessages(50):iter() do
+			if m.author == m.client.user or string.find(msg.content, settings.prefix or msg.client.prefix[1]) then
+				table.insert(ids, m.id)
 			end
 		end
+		msg.channel:bulkDelete(ids)
 	end
 }
