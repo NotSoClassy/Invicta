c1739b34b85c3ef1ff562dac96a5029b4909f450
NotSoClassy <NotSoClassy357@gmail.com>
2020-12-08 20:49:54 -0500

    embarrassing

--- src/commands/choose.lua
+++ src/commands/choose.lua
@@ -3,7 +3,7 @@ return {
 	description = 'Chooses a random choice',
 	example = '<choice1> <choice2> [choice3] ...',
 	execute = function(msg, args)
-		if #args <= 0 then return msg:reply('You didn\'t ask anything') end
+		if #args < 2 then return msg:reply('You need to provide at least 2 choices.') end
 		msg:reply(args[math.random(#args)])
 	end
 }
