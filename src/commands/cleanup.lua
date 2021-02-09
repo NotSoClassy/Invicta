91229c6140f4ac8af69575dd2e4e20fa738ec1c4
canada <notsoclassy357@gmail.com>
2021-02-09 13:01:40 -0500

    bug fix again

--- src/commands/cleanup.lua
+++ src/commands/cleanup.lua
@@ -8,7 +8,7 @@ return {
 	execute = function(msg, _, settings)
 		local ids = {}
 		for m in msg.channel:getMessages(50):iter() do
-			if m.author == m.client.user or string.find(msg.content, settings.prefix or msg.client.prefix[1]) and util.canBulkDelete(msg) then
+			if m.author == m.client.user or string.find(msg.content, settings.prefix or msg.client.prefix[1]) == 1 and util.canBulkDelete(msg) then
 				table.insert(ids, m.id)
 			end
 		end
