1216aafe3d4d292659b02f459426c02b2ce41dae
canada <notsoclassy357@gmail.com>
2021-03-10 16:18:37 -0500

    bug fix

--- src/commands/choose.lua
+++ src/commands/choose.lua
@@ -3,7 +3,8 @@ return {
 	description = 'Chooses a random choice',
 	args = {{ name = 'choices', value = '...' }},
 	execute = function(msg, args)
+		if not args.choices then return msg:reply('You didn\'t provide any choices') end
 		if #args.choices < 2 then return msg:reply('You need to provide at least 2 choices.') end
-		msg:reply(args[math.random(#args)])
+		msg:reply(args.choices[math.random(#args.choices)])
 	end
 }
